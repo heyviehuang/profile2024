d5b7c53bc1416f60e3e441fee97f22199a05ac7a
Sally Huang <90699601+heyviehuang@users.noreply.github.com>
2025-01-02 18:18:07 +0800

    網頁架構大更動

--- js/all.js
+++ js/all.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('.scrollTop').click(function(e) {
+    $('.scrollTop').click(function (e) {
         e.preventDefault();
         var target = $(this).attr('href');
         var targetPos = $(target).offset().top;
@@ -9,33 +9,34 @@ $(document).ready(function() {
         }, 700);
     });
 
+    // 光塵特效
+    // $(".snow").let_it_snow({
+    //     windPower: 0.01,
+    //     speed: 0.001,
+    //     count: 20,
+    //     size: 3,
+    //     image: "./img/img-bgItem-snow.png"
+    // });
+    // $("canvas.flake").let_it_snow({
+    //     windPower: 2,
+    //     speed: 0.5,
+    //     count: 2,
+    //     size: 10,
+    //     image: "./img/img-bgItem-snow.png"
+    // });
 
-    $(".snow").let_it_snow({
-        windPower: 0.01,
-        speed: 0.001,
-        count: 20,
-        size: 3,
-        image: "img/img-bgItem-snow.png"
-    });
-    $("canvas.flake").let_it_snow({
-        windPower: 2,
-        speed: 0.5,
-        count: 2,
-        size: 10,
-        image: "img/img-bgItem-snow.png"
-    });
+    // $(".anim-sparkle").sparkle({
+    //     color: "#FFFFFF",
+    //     count: 30,
+    //     overlap: 0,
+    //     speed: 1,
+    //     minSize: 4,
+    //     maxSize: 10,
+    //     direction: "both"
+    // });
 
-    $(".anim-sparkle").sparkle({
-        color: "#FFFFFF",
-        count: 30,
-        overlap: 0,
-        speed: 1,
-        minSize: 4,
-        maxSize: 10,
-        direction: "both"
-    });
 
-    $('.btnTop').click(function(event) {
+    $('.btnTop').click(function (event) {
         event.preventDefault();
         $('html,body').animate({
             scrollTop: 0
@@ -51,17 +52,17 @@ $(document).ready(function() {
     }
     $(window).scroll(showBtnCondition);
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         var scrollPos = $(window).scrollTop();
         var windowHeight = $(window).height();
         // animated
-        $('.animT,.animR,.animB,.animL,.animM').each(function() {
+        $('.animT,.animR,.animB,.animL,.animM').each(function () {
             var thisPos = $(this).offset().top;
             if ((windowHeight + scrollPos) >= thisPos) {
                 $(this).addClass('fadeIn');
             }
         });
-        $('.blurIncontainer').each(function() {
+        $('.blurIncontainer').each(function () {
             var thisPos = $(this).offset().top;
             if ((windowHeight + scrollPos) >= thisPos) {
                 $(this).find('char,.char01').css({ 'animation-name': 'blurInAnim', 'animation-duration': '1.6s', 'animation-delay': '0s', 'animation-fill-mode': 'forwards' });
@@ -72,9 +73,9 @@ $(document).ready(function() {
     });
 
 
-    $(function() {
+    $(function () {
         // 選擇所有的 .infiniteslide 元素
-        $('.infiniteslide').each(function(index, element) {
+        $('.infiniteslide').each(function (index, element) {
             var direction = $(element).data('direction');
             var clone = $(element).data('clone');
 
@@ -118,7 +119,7 @@ $(document).ready(function() {
     $("#tab2").hide();
 
     // 按鈕切換內容
-    $(".tab-btn").click(function() {
+    $(".tab-btn").click(function () {
         var tabId = $(this).attr("id");
 
         // 使用 if 條件判斷來切換內容
@@ -136,9 +137,9 @@ $(document).ready(function() {
     });
 
 
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         let sections = document.querySelectorAll('.dynamicBG-container');
-        sections.forEach(function(section, index) {
+        sections.forEach(function (section, index) {
             let rect = section.getBoundingClientRect();
             if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
                 // Add blur class when the section is in view
@@ -149,7 +150,7 @@ $(document).ready(function() {
             }
         });
         if (window.scrollY <= 0) {
-            sections.forEach(function(section) {
+            sections.forEach(function (section) {
                 section.classList.remove('blur');
             });
         }
