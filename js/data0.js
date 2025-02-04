10e450064d2ab138605f7c168690b187d39ecd47
Sally Huang <90699601+heyviehuang@users.noreply.github.com>
2025-02-04 18:21:42 +0800

    全屏滾動CSS整理

--- js/data0.js
+++ js/data0.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#pagepiling').pagepiling({
         menu: '#menu',
         anchors: ['page1', 'page2', 'page3', 'page4'],
-        sectionsColor: ['#bfda00', '#2ebe21', '#2C3E50', '#51bec4'],
+        sectionsColor: ['#bfda00', '#3a3a3c', '#8e8e93', '#51bec4'],
         navigation: {
             'position': 'right',
             'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4']
