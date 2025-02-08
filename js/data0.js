5e6c5cdc76bd016113d6fc7ab5b97ea46761ced2
Sally Huang <90699601+heyviehuang@users.noreply.github.com>
2025-02-08 17:46:14 +0800

    index設計&主題變數化
    -index三區塊布局設計
    -新增霓虹燈文字效果
    -新增常態按鈕
    -頁面主題變數化調整

--- js/data0.js
+++ js/data0.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#pagepiling').pagepiling({
         menu: '#menu',
         anchors: ['page1', 'page2', 'page3', 'page4'],
-        sectionsColor: ['#bfda00', '#3a3a3c', '#8e8e93', '#51bec4'],
+        sectionsColor: ['#ffffff', '#1c1c1e', '#ffffff', '#51bec4'],
         navigation: {
             'position': 'right',
             'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4']
