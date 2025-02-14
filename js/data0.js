06aa4f81850ae857575473dfe6bd1804912c9570
Sally Huang <90699601+heyviehuang@users.noreply.github.com>
2025-02-14 18:21:47 +0800

    index setion1&4 編輯
    -新增亂數按鈕&表單
    -更新menu&footer
    -關閉google翻譯提示

--- js/data0.js
+++ js/data0.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#pagepiling').pagepiling({
         menu: '#menu',
         anchors: ['page1', 'page2', 'page3', 'page4'],
-        sectionsColor: ['#ffffff', '#1c1c1e', '#ffffff', '#51bec4'],
+        sectionsColor: ['#ffffff', '#1c1c1e', '#ffffff', '#ffffff'],
         navigation: {
             'position': 'right',
             'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4']
